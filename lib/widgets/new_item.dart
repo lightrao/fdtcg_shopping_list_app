db7a8049bacc9e5f652bcdb827f6f581a0bbff79
lightrao <lightrao@hotmail.com>
2024-10-03 08:45:12 +0800

    A Form-aware Dropdown Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,4 @@
+import 'package:fdtcg_shopping_list_app/data/categories.dart';
 import 'package:flutter/material.dart';
 
 class NewItem extends StatefulWidget {
@@ -29,6 +30,46 @@ class _NewItemState extends State<NewItem> {
                 validator: (value) {
                   return 'Demo ...';
                 },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
